5c755385424a175db5ca0ceea09ba58291e4d8e6
gurungshashak <gurungshashak@gmail.com>
2025-11-07 15:52:32 -0500

    little fixes

--- src/main/java/com/pluralsight/sandwich/Drink.java
+++ src/main/java/com/pluralsight/sandwich/Drink.java
@@ -2,8 +2,6 @@ package com.pluralsight.sandwich;
 
 import com.pluralsight.typeandsize.DrinkSize;
 
-import java.util.Scanner;
-
 public class Drink {
     private DrinkSize size;
     private String flavor;
@@ -11,6 +9,17 @@ public class Drink {
     public Drink(DrinkSize size, String flavor) {
         this.size = size;
         this.flavor = flavor;
+    }
+    public double getPrice() {
+        return switch (size){
+            case Small -> 1.00;
+            case Medium -> 1.50;
+            case Large -> 2.00;
+        };
+    }
+
+    public String toString() {
+        return size + " " + flavor + ": $" + String.format("%.2f", getPrice());
     }
 
 }
